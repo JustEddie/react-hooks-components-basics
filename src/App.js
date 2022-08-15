22a822f18b49089c7aee942feba23c1ea4a11de8
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-08-15 21:05:14 +1000

    practice react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Practice from "./Practice";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Practice />
     </div>
   );
 }
---
+++ src/Practice.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Practice() {
+  return (
+    <div>I'm practicing writing component</div>
+  );
+}
+
+export default Practice;
